c1d2059b3f2001c53593224b029364c30a17b5a0
Danielle Donnelly <145680391+danielledonnelly@users.noreply.github.com>
2025-06-19 07:45:01 -0230

    commented out example commands (thank you for the examples sven)

--- commands/longping.js
+++ commands/longping.js
@@ -1,23 +1,23 @@
 // Use this for all other commands.
 
-import { SlashCommandBuilder } from 'discord.js';
+// import { SlashCommandBuilder } from 'discord.js';
 
-import { setTimeout as wait } from 'node:timers/promises';
+// import { setTimeout as wait } from 'node:timers/promises';
 
-export default {
+// export default {
 
-    data: new SlashCommandBuilder()
-        .setName('pinglong')
-        .setDescription('Replies with pong after a delay!')
-        .addIntegerOption(option => option.setName('seconds')
-            .setDescription('How long in seconds to wait')
-            .setRequired(true)
-        ),
+//     data: new SlashCommandBuilder()
+//         .setName('pinglong')
+//         .setDescription('Replies with pong after a delay!')
+//         .addIntegerOption(option => option.setName('seconds')
+//             .setDescription('How long in seconds to wait')
+//             .setRequired(true)
+//         ),
 
-    execute: async (interaction) => {
-        const seconds = interaction.options.getInteger('seconds') 
-        await interaction.deferReply();
-        await wait(seconds * 1000);
-        await interaction.editReply('Pong!');
-    }
-}
+//     execute: async (interaction) => {
+//         const seconds = interaction.options.getInteger('seconds') 
+//         await interaction.deferReply();
+//         await wait(seconds * 1000);
+//         await interaction.editReply('Pong!');
+//     }
+// }
--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,11 @@
 // Use this for a fast running command. A fast running command is a command that finishes within 3 seconds
-import { SlashCommandBuilder } from 'discord.js';
+// import { SlashCommandBuilder } from 'discord.js';
 
-export default {
-    data: new SlashCommandBuilder()
-        .setName('ping')
-        .setDescription('Replies with Pong!'),
-    execute: async (interaction) => {
-        await interaction.reply('Pong!');
-    }
-}
+// export default {
+//     data: new SlashCommandBuilder()
+//         .setName('ping')
+//         .setDescription('Replies with Pong!'),
+//     execute: async (interaction) => {
+//         await interaction.reply('Pong!');
+//     }
+// }
